aa81409d926ab8515790d884073371d38a09e1eb
HarunBuyuktepe <38882108+HarunBuyuktepe@users.noreply.github.com>
2019-05-16 23:58:42 +0300

    Add files via upload

--- firstQuestion/deneme.py
+++ firstQuestion/deneme.py
@@ -1,4 +1,5 @@
 import matplotlib.pylab as plt
+import pic as pic
 from PIL import Image
 from numpy import pi, exp, sqrt
 import numpy as np
@@ -20,123 +21,128 @@ a = np.array(kernel)
 a.transpose()
 print(a)
 
-pix = img.load() # Get the width and hight of the image for iterating over
+pix = np.asarray(img)# Get the width and hight of the image for iterating over
+pix = img.load()
+pix=list(img.getdata())
 
+
+
+print(pix[40][40])
 width, height = img.size
 sum=0
 array = (np.random.rand(height, width) * 256).astype(np.uint8)
 for i in range (height): #traverses through height of the image
     for j in range (width): #traverses through width of the image
         try:
-            sum+=kernel[0][0]*pix[j - 2 , i - 2]
+            sum+=kernel[0][0]*pix[j - 2 ][ i - 2]
         except:
             sum+=0
         try:
-            sum+=kernel[1][0]*pix[j - 2 , i - 1]
+            sum+=kernel[1][0]*pix[j - 2 ][ i - 1]
         except:
             sum+=0
         try:
-            sum+=kernel[2][0]*pix[j - 2 , i]
+            sum+=kernel[2][0]*pix[j - 2 ][i]
         except:
             sum+=0
         try:
-            sum+=kernel[3][0]*pix[j - 2 , i + 1]
+            sum+=kernel[3][0]*pix[j - 2 ][i + 1]
         except:
             sum+=0
         try:
-            sum+=kernel[4][0]*pix[j - 2 , i + 2]
+            sum+=kernel[4][0]*pix[j - 2 ][i + 2]
         except:
             sum+=0
         try:
-            sum+=kernel[0][1]*pix[j - 1 , i - 2]
+            sum+=kernel[0][1]*pix[j - 1 ][i - 2]
         except:
             sum+=0
         try:
-            sum+=kernel[1][1]*pix[j - 1 , i - 1]
+            sum+=kernel[1][1]*pix[j - 1 ][i - 1]
         except:
             sum+=0
         try:
-            sum+=kernel[2][1]*pix[j - 1 , i]
+            sum+=kernel[2][1]*pix[j - 1 ][i]
         except:
             sum+=0
         try:
-            sum+=kernel[3][1]*pix[j - 1 , i + 1]
+            sum+=kernel[3][1]*pix[j - 1][ i + 1]
         except:
             sum+=0
         try:
-            sum+=kernel[4][1]*pix[j - 1 , i + 2]
+            sum+=kernel[4][1]*pix[j - 1 ][ i + 2]
         except:
             sum+=0
         try:
-            sum+=kernel[0][2]*pix[j , i - 2]
+            sum+=kernel[0][2]*pix[j ][ i - 2]
         except:
             sum+=0
         try:
-            sum+=kernel[1][2]*pix[j , i - 1]
+            sum+=kernel[1][2]*pix[j ][ i - 1]
         except:
             sum+=0
         try:
-            sum+=kernel[2][2]*pix[j , i]
+            sum+=kernel[2][2]*pix[j ][ i]
         except:
             sum+=0
         try:
-            sum+=kernel[3][2]* pix[j , i + 1]
+            sum+=kernel[3][2]* pix[j ][ i + 1]
         except:
             sum+=0
         try:
-            sum+=kernel[4][2]*pix[j , i + 2]
+            sum+=kernel[4][2]*pix[j ][ i + 2]
         except:
             sum+=0
         try:
-            sum+=kernel[0][3]*pix[j + 1 , i - 2]
+            sum+=kernel[0][3]*pix[j + 1 ][ i - 2]
         except:
             sum+=0
         try:
-            sum+=kernel[1][3]* pix[j + 1 , i - 1]
+            sum+=kernel[1][3]* pix[j + 1 ][ i - 1]
         except:
             sum+=0
         try:
-            sum+=kernel[2][3]*pix[j + 1 , i]
+            sum+=kernel[2][3]*pix[j + 1 ][ i]
         except:
             sum+=0
         try:
-            sum+=kernel[3][3]*pix[j + 1 , i + 1]
+            sum+=kernel[3][3]*pix[j + 1 ][ i + 1]
         except:
             sum+=0
         try:
-            sum+=kernel[4][3]*pix[j + 1 , i + 2]
+            sum+=kernel[4][3]*pix[j + 1 ][ i + 2]
         except:
             sum+=0
         try:
-            sum+=kernel[0][4]*pix[j + 2 , i - 2]
+            sum+=kernel[0][4]*pix[j + 2 ][ i - 2]
         except:
             sum+=0
         try:
-            sum+=kernel[1][4]*pix[j + 2 , i - 1]
+            sum+=kernel[1][4]*pix[j + 2 ][ i - 1]
         except:
             sum+=0
         try:
-            sum+=kernel[2][4]*pix[j + 2 , i]
+            sum+=kernel[2][4]*pix[j + 2 ][ i]
         except:
             sum+=0
         try:
-            sum+=kernel[3][4]*pix[j + 2 , i + 1]
+            sum+=kernel[3][4]*pix[j + 2][  i + 1]
         except:
             sum+=0
         try:
-            sum+=kernel[4][4]*pix[j + 2 , i + 2]
+            sum+=kernel[4][4]*pix[j + 2 ][ i + 2]
         except:
             sum+=0
-        pix[j,i]=int(sum)
+        pix[j][i]=int(sum)
         #print(sum)
         sum=0
 
 
 
 
-
-
-img = Image.fromarray(pix)
+print(pix[40][40])
+#img=pic.putdata(pix,'P')
+img = Image.fromarray(pix,'P')
 img.save('test.png')
 
 

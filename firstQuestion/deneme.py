e3216d49c638f3c9aece5fce881ea1670c145cdf
HarunBuyuktepe <38882108+HarunBuyuktepe@users.noreply.github.com>
2019-05-17 22:45:23 +0300

    Add files via upload

--- firstQuestion/deneme.py
+++ firstQuestion/deneme.py
@@ -4,146 +4,135 @@ from PIL import Image
 from numpy import pi, exp, sqrt
 import numpy as np
 
+
+def filter(img,kernel,name):
+    pix = np.array(img)
+    width, height = img.size  # Get the width and hight of the image for iterating over
+    print(width, height)
+    sum = 0
+    for i in range(height):  # traverses through height of the image
+        for j in range(width):  # traverses through width of the image
+            try:
+                sum += kernel[0][0] * pix[i - 2][j - 2]
+            except:
+                sum += 0
+            try:
+                sum += kernel[1][0] * pix[i - 2][j - 1]
+            except:
+                sum += 0
+            try:
+                sum += kernel[2][0] * pix[i - 2][j]
+            except:
+                sum += 0
+            try:
+                sum += kernel[3][0] * pix[i - 2][j + 1]
+            except:
+                sum += 0
+            try:
+                sum += kernel[4][0] * pix[i - 2][j + 2]
+            except:
+                sum += 0
+            try:
+                sum += kernel[0][1] * pix[i - 1][j - 2]
+            except:
+                sum += 0
+            try:
+                sum += kernel[1][1] * pix[i - 1][j - 1]
+            except:
+                sum += 0
+            try:
+                sum += kernel[2][1] * pix[i - 1][j]
+            except:
+                sum += 0
+            try:
+                sum += kernel[3][1] * pix[i - 1][j + 1]
+            except:
+                sum += 0
+            try:
+                sum += kernel[4][1] * pix[i - 1][j + 2]
+            except:
+                sum += 0
+            try:
+                sum += kernel[0][2] * pix[i][j - 2]
+            except:
+                sum += 0
+            try:
+                sum += kernel[1][2] * pix[i][j - 1]
+            except:
+                sum += 0
+            try:
+                sum += kernel[2][2] * pix[i][j]
+            except:
+                sum += 0
+            try:
+                sum += kernel[3][2] * pix[i][j + 1]
+            except:
+                sum += 0
+            try:
+                sum += kernel[4][2] * pix[i][j + 2]
+            except:
+                sum += 0
+            try:
+                sum += kernel[0][3] * pix[i + 1][j - 2]
+            except:
+                sum += 0
+            try:
+                sum += kernel[1][3] * pix[i + 1][j - 1]
+            except:
+                sum += 0
+            try:
+                sum += kernel[2][3] * pix[i + 1][j]
+            except:
+                sum += 0
+            try:
+                sum += kernel[3][3] * pix[i + 1][j + 1]
+            except:
+                sum += 0
+            try:
+                sum += kernel[4][3] * pix[i + 1][j + 2]
+            except:
+                sum += 0
+            try:
+                sum += kernel[0][4] * pix[i + 2][j - 2]
+            except:
+                sum += 0
+            try:
+                sum += kernel[1][4] * pix[i + 2][j - 1]
+            except:
+                sum += 0
+            try:
+                sum += kernel[2][4] * pix[i + 2][j]
+            except:
+                sum += 0
+            try:
+                sum += kernel[3][4] * pix[i + 2][j + 1]
+            except:
+                sum += 0
+            try:
+                sum += kernel[4][4] * pix[i + 2][j + 2]
+            except:
+                sum += 0
+
+            pix[i][j] = int(sum)
+            # print(sum)
+            sum = 0
+
+    img = Image.fromarray(pix, 'P')
+    img.save(name+'.png')
+
+img = Image.open('image0.png').convert('L')
+
+#gaussian kerneli yani filtresi
 s, k = 1, 2 #  generate a (2k+1)x(2k+1) gaussian kernel with mean=0 and sigma = s
 probs = [exp(-z*z/(2*s*s))/sqrt(2*pi*s*s) for z in range(-k,k+1)]
 kernel = np.outer(probs, probs)
 
-img = Image.open('firstQuestion/image0.png').convert('L')
-#Image._show(img)
-value=list(img.getdata())
-print(value)
-
-s, k = 1, 2 #  generate a (2k+1)x(2k+1) gaussian kernel with mean=0 and sigma = s
-probs = [exp(-z*z/(2*s*s))/sqrt(2*pi*s*s) for z in range(-k,k+1)]
-kernel = np.outer(probs, probs)
-
-a = np.array(kernel)
-a.transpose()
-print(a)
-
-pix = np.asarray(img)# Get the width and hight of the image for iterating over
-pix = img.load()
-pix=list(img.getdata())
+gaussian = np.array(kernel)
+gaussian.transpose()
+print(gaussian)
 
 
-
-print(pix[40][40])
-width, height = img.size
-sum=0
-array = (np.random.rand(height, width) * 256).astype(np.uint8)
-for i in range (height): #traverses through height of the image
-    for j in range (width): #traverses through width of the image
-        try:
-            sum+=kernel[0][0]*pix[j - 2 ][ i - 2]
-        except:
-            sum+=0
-        try:
-            sum+=kernel[1][0]*pix[j - 2 ][ i - 1]
-        except:
-            sum+=0
-        try:
-            sum+=kernel[2][0]*pix[j - 2 ][i]
-        except:
-            sum+=0
-        try:
-            sum+=kernel[3][0]*pix[j - 2 ][i + 1]
-        except:
-            sum+=0
-        try:
-            sum+=kernel[4][0]*pix[j - 2 ][i + 2]
-        except:
-            sum+=0
-        try:
-            sum+=kernel[0][1]*pix[j - 1 ][i - 2]
-        except:
-            sum+=0
-        try:
-            sum+=kernel[1][1]*pix[j - 1 ][i - 1]
-        except:
-            sum+=0
-        try:
-            sum+=kernel[2][1]*pix[j - 1 ][i]
-        except:
-            sum+=0
-        try:
-            sum+=kernel[3][1]*pix[j - 1][ i + 1]
-        except:
-            sum+=0
-        try:
-            sum+=kernel[4][1]*pix[j - 1 ][ i + 2]
-        except:
-            sum+=0
-        try:
-            sum+=kernel[0][2]*pix[j ][ i - 2]
-        except:
-            sum+=0
-        try:
-            sum+=kernel[1][2]*pix[j ][ i - 1]
-        except:
-            sum+=0
-        try:
-            sum+=kernel[2][2]*pix[j ][ i]
-        except:
-            sum+=0
-        try:
-            sum+=kernel[3][2]* pix[j ][ i + 1]
-        except:
-            sum+=0
-        try:
-            sum+=kernel[4][2]*pix[j ][ i + 2]
-        except:
-            sum+=0
-        try:
-            sum+=kernel[0][3]*pix[j + 1 ][ i - 2]
-        except:
-            sum+=0
-        try:
-            sum+=kernel[1][3]* pix[j + 1 ][ i - 1]
-        except:
-            sum+=0
-        try:
-            sum+=kernel[2][3]*pix[j + 1 ][ i]
-        except:
-            sum+=0
-        try:
-            sum+=kernel[3][3]*pix[j + 1 ][ i + 1]
-        except:
-            sum+=0
-        try:
-            sum+=kernel[4][3]*pix[j + 1 ][ i + 2]
-        except:
-            sum+=0
-        try:
-            sum+=kernel[0][4]*pix[j + 2 ][ i - 2]
-        except:
-            sum+=0
-        try:
-            sum+=kernel[1][4]*pix[j + 2 ][ i - 1]
-        except:
-            sum+=0
-        try:
-            sum+=kernel[2][4]*pix[j + 2 ][ i]
-        except:
-            sum+=0
-        try:
-            sum+=kernel[3][4]*pix[j + 2][  i + 1]
-        except:
-            sum+=0
-        try:
-            sum+=kernel[4][4]*pix[j + 2 ][ i + 2]
-        except:
-            sum+=0
-        pix[j][i]=int(sum)
-        #print(sum)
-        sum=0
-
-
-
-
-print(pix[40][40])
-#img=pic.putdata(pix,'P')
-img = Image.fromarray(pix,'P')
-img.save('test.png')
+filter(img,gaussian,"gaussian")
 
 
 
